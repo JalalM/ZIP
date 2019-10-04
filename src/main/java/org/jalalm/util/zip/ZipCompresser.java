45a860a6cc9aaae566b6101036f8dcbb49b9ca2d
Jalal Majed AlRukhaimi <conany1@gmail.com>
2019-10-04 20:55:58 +0300

    Add ZipCompressor class

---
+++ src/main/java/org/jalalm/util/zip/ZipCompresser.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2019 Jalal.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jalalm.util.zip;
+
+import java.io.File;
+
+/**
+ *
+ * @author Jalal
+ */
+public class ZipCompresser {
+    
+    /**
+     *
+     * @param file
+     */
+    public static void CompressFile(File file){
+    }
+    
+    /**
+     *
+     * @param path
+     */
+    public static void CompressFile(String path){
+    }
+    
+    /**
+     *
+     * @param zipName
+     * @param files
+     */
+    public static void CompressFiles(String zipName, File[] files){
+    }
+    
+    /**
+     *
+     * @param zipName
+     * @param files
+     */
+    public static void CompressFiles(String zipName, String[] files){
+    }
+    
+    /**
+     *
+     * @param file
+     */
+    public static void CompressDir(File file){
+    }
+    
+    /**
+     *
+     * @param path
+     */
+    public static void CompressDir(String path){
+    }
+}
